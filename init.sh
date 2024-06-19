2eb79456f5c550ea3c02804adba031ea3c51632b
Mahmoud Saad <me@mahmoudsaad.net>
2024-06-19 19:51:28 +0300

    minor fix

--- init.sh
+++ init.sh
@@ -29,13 +29,6 @@ read -p "Enter your GitHub email address: " email
 echo "Generating SSH key..."
 ssh-keygen -t ed25519 -C "$email" -f ~/.ssh/id_ed25519_github -N ""
 
-# Start the ssh-agent in the background
-# Add the SSH key to the ssh-agent
-echo "Starting ssh-agent..."
-echo "Adding SSH key to ssh-agent..."
-eval "$(ssh-agent -s)"
-ssh-add ~/.ssh/id_ed25519_github
-
 # Copy ssh config file
 echo "Copying ssh config file..."
 cp ssh_config ~/.ssh/config
